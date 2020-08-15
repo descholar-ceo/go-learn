0ad79ce4d741f92f35523e0f380e85dcd7b830b3
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-15 08:57:27 +0200

    Add the print statement of z

--- 7-types/types.go
+++ 7-types/types.go
@@ -12,4 +12,5 @@ func main() {
 	fmt.Printf("The type of j is %T:\n", j)
 	fmt.Printf("The type of x is %T:\n", x)
 	fmt.Printf("The type of y is %T:\n", y)
+	fmt.Printf("The type of z is %T:\n", z)
 }
