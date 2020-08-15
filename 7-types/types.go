149421ee98856d148d1e6837bd431833b15d1de9
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-15 08:59:06 +0200

    Refactor

--- 7-types/types.go
+++ 7-types/types.go
@@ -8,9 +8,9 @@ func main() {
 	x := 12.3
 	y := "Some people are just funny!"
 	z := 'c'
-	fmt.Printf("The type of i is %T:\n", i)
-	fmt.Printf("The type of j is %T:\n", j)
-	fmt.Printf("The type of x is %T:\n", x)
-	fmt.Printf("The type of y is %T:\n", y)
-	fmt.Printf("The type of z is %T:\n", z)
+	fmt.Printf("The type of i is: %T\n", i)
+	fmt.Printf("The type of j is: %T\n", j)
+	fmt.Printf("The type of x is: %T\n", x)
+	fmt.Printf("The type of y is: %T\n", y)
+	fmt.Printf("The type of z is: %T\n", z)
 }
