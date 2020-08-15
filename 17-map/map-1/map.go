a83e4f8016807a9441dc892dd056e3695d438c1e
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-16 00:26:15 +0200

    Add map example

--- 17-map/map-1/map.go
+++ 17-map/map-1/map.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 type loca struct {
 	Lat, Long float64
 }
 
-func main() {}
+func main() {
+	m := make(map[string]loca)
+	m["kigali butare"] = loca{2212, 234}
+	fmt.Println(m["kigali butare"])
+}
