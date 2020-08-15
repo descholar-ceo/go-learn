17eb01bcfc6d8acf4b5821bc2b3475ace553ef12
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-15 23:30:07 +0200

    Add more elt to the slice

--- 15-slices/slices-8/slice-append.go
+++ 15-slices/slices-8/slice-append.go
@@ -6,5 +6,12 @@ func printSlice(name string, slice []int) {
 	fmt.Printf("The slice %s has length of %d and capacity of %d, and the slice itself is %v\n", name, len(slice), cap(slice), slice)
 }
 func main() {
-
+	var slice1 []int
+	printSlice("slice1", slice1)
+	slice1 = append(slice1, 0)
+	printSlice("slice1", slice1)
+	slice1 = append(slice1, 1)
+	printSlice("slice1", slice1)
+	slice1 = append(slice1, 2, 3, 4, 5)
+	printSlice("slice1", slice1)
 }
