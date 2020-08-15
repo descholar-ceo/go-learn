efdb2f19ba4f0e8d33d3bfe997116c7c7cc64adb
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-15 15:38:35 +0200

    All switch case logic

--- 10-switch-case/switch-case-2/switch-case-ev.go
+++ 10-switch-case/switch-case-2/switch-case-ev.go
@@ -11,5 +11,11 @@ func main() {
 	switch time.Saturday {
 	case today + 0:
 		fmt.Println("It's today")
+	case today + 1:
+		fmt.Println("It's in one days")
+	case today + 2:
+		fmt.Println("It's in two days")
+	default:
+		fmt.Println("It's far from today")
 	}
 }
