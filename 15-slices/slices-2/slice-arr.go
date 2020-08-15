7fdeea83497cfd502fb3e739127cc591a713f737
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-15 21:11:19 +0200

    Add modification to the slice

--- 15-slices/slices-2/slice-arr.go
+++ 15-slices/slices-2/slice-arr.go
@@ -10,5 +10,13 @@ func main() {
 		"lname",
 		"nezago",
 	}
-	fmt.Println(arr)
+	fmt.Println("The original array is : ", arr)
+	a := arr[0:2]
+	b := arr[0:5]
+	fmt.Println("The slice a is : ", a)
+	fmt.Println("The slice b is : ", b)
+	b[1] = "mugirase"
+	fmt.Println("The new slice a is : ", a)
+	fmt.Println("The new slice b is : ", b)
+	fmt.Println("The new array arr is : ", arr)
 }
