bf8da7d8fa08666cd2b26cbc7deb5723659ebe86
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-17 19:26:51 +0200

    Add hypo function

---
+++ 18-functions/functions-1/functions.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func main() {
+	hypo := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypo(5, 12))
+}
