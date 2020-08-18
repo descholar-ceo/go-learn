e75347ebd4d5a815f4249335c4b7958e15d339ee
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-18 10:57:52 +0200

    Add func main

--- 19-methods/methods-3/methods-no-struct.go
+++ 19-methods/methods-3/methods-no-struct.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type mFloat float64
 
-func (m mFloat) divideWithFloat(divider int) mFloat {
+func (m mFloat) divideWith(divider int) mFloat {
 	return (mFloat(divider) / m)
+}
+
+func main() {
+	num := mFloat(math.Pi)
+	fmt.Printf("The result is : %f", num.divideWith(4))
 }
