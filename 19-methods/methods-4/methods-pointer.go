e5413230b5080395095d1cea634922659d8cc63e
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-18 13:17:24 +0200

    Add method and pointers

--- 19-methods/methods-4/methods-pointer.go
+++ 19-methods/methods-4/methods-pointer.go
@@ -1,10 +1,25 @@
 package main
 
-type mInt int
+import (
+	"fmt"
+	"math"
+)
 
-func (m mInt) absolute() mInt {
-	if m < 0 {
-		return (+m)
-	}
-	return m
+type vertex struct {
+	X, Y float64
+}
+
+func (v vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
